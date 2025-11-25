05ae33fd13033ba707e4bfac1c53d3c8fff01b8c
LaQuisha Cain <llcain2012@gmail.com>
2025-11-24 21:07:50 -0600

    reference file in sst.config.ts has no red underline

--- Affect-Tracking-App/sst.config.ts
+++ Affect-Tracking-App/sst.config.ts
@@ -1,27 +1,2 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
-
-
-import * as sst from "sst";
-
-export default $config({
-  app(input) {
-    return {
-      name: "affect-tracker",
-      home: "aws",
-      version: ">=3.0.0",
-    };
-  },
-
-  async run() {
-    const site = new sst.aws.StaticSite("site", {
-      path: ".",
-      buildCommand: "npm run build",
-      buildOutput: "dist",
-    });
-
-    return {
-      SiteUrl: site.url,
-    };
-  },
-});
