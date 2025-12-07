73fa243d1d6858b32eb0a6c9830687c588f6895d
LaQuisha Cain <llcain2012@gmail.com>
2025-12-07 16:57:48 -0600

    working on sst.config.ts file - no red line under reference path

--- Affect-Tracking-App/sst.config.ts
+++ Affect-Tracking-App/sst.config.ts
@@ -1,2 +1,15 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+export default $config({
+    // app's config
+    app(input) {
+        return {
+            name:"Affect-Tracking-App",
+            home: "aws"
+        };
+    },
+
+    // app's resources
+
+    async run() {}
+});
