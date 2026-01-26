d29d9fc92af6b5ddf7054d98cf9423539533247c
LaQuisha Cain <llcain2012@gmail.com>
2026-01-26 07:17:58 -0600

    working on Mood Chart component

--- Affect-Tracking-App/src/pages/MoodChart.vue
+++ Affect-Tracking-App/src/pages/MoodChart.vue
@@ -1,29 +1,44 @@
 <script setup>
 import {ref} from 'vue';
 
-const username = ref('');
+const userName = ref('');
 const moods = ref(['happy', 'nuetral', 'sad']);
+const names = ref([]);
+
+const addName = () => {
+  if (!userName.value.trim()) return
+
+  names.value.push(userName.value)
+  userName.value = ''
+}
+
 
 </script>
 
 <template lang="pug">
 // create legend
-
 h1 Legend
+
 ul.legend
   li Happy 😀😀😀
   li Neutral 😑😑😑
   li Sad 😒😒😒
 
-  input(
-    type="text"
-    placeholder="Enter Your Name"
-    )
-  button Add User
+input(
+  type="text"
+  v-model="userName"
+  placeholder="Enter Your Name"
+)
+button(@click="addName") Add User
 
-  div#mood-container
-    h2 Moods
-    div.moodList
+div#mood-container
+  h2 Moods
+
+  div#moodList
+    ul
+      li(v-for="(name, index) in names" :key="index")
+        {{ name }}
+
 </template>
 
 <style scoped>
